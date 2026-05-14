7668c8ecdf5ca771a1b7fe3ff2be9606da4a7161
BogdanSorinescu <bogdansorinescu@gmail.com>
2026-05-14 17:56:55 +0200

    added layout and header styling

--- src/components/liquidGlass.jsx
+++ src/components/liquidGlass.jsx
@@ -1,19 +1,25 @@
 import LiquidGlassModule from "liquid-glass-react";
 
-import './liquidGlass.css';
+import "./liquidGlass.css";
 
 const LiquidGlass = LiquidGlassModule.default ?? LiquidGlassModule;
 
-
-export default function LiquidGlassWrapper({ children }) {
+export default function LiquidGlassWrapper({
+  children,
+  className = "intro-glass",
+  style = { position: "fixed", top: "10%", left: "50%" },
+  padding = "16px 32px",
+  cornerRadius = 999,
+}) {
   return (
-    
-      <LiquidGlass
-        className="intro-glass"
-        padding="25px 50px"
-        cornerRadius={999}
+    <LiquidGlass
+      className={className}
+      style={style}
+      padding={padding}
+      cornerRadius={cornerRadius}
+      overLight
     >
       <div className="intro-content">{children}</div>
-      </LiquidGlass>
+    </LiquidGlass>
   );
 }
