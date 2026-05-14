e59d7923cc6473fd7d5812550188a5c56bc7b678
BogdanSorinescu <bogdansorinescu@gmail.com>
2026-05-14 17:10:25 +0200

    added minor styling

--- src/components/liquidGlass.jsx
+++ src/components/liquidGlass.jsx
@@ -1,23 +1,19 @@
 import LiquidGlassModule from "liquid-glass-react";
-import ImageOfMe from '../assets/ImageOfMe.png';
+
 import './liquidGlass.css';
 
 const LiquidGlass = LiquidGlassModule.default ?? LiquidGlassModule;
 
 
-export default function Index() {
+export default function LiquidGlassWrapper({ children }) {
   return (
-    <div id="testPage">
-      <img src={ImageOfMe.src} alt="Me" className="profile-image" />
+    
       <LiquidGlass
         className="intro-glass"
         padding="25px 50px"
         cornerRadius={999}
-      >
-        <div className="intro-content">
-          <h2>Hello, my name is Bogdan!</h2>
-        </div>
+    >
+      <div className="intro-content">{children}</div>
       </LiquidGlass>
-    </div>
   );
 }
